6e43b09698656aeb730b76574c8c6d2a6a404c70
ecomnazar <ecomnazar.me@gmail.com>
2024-06-01 14:31:46 +0500

    [added]: curved line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <OurTarget />
       <OurAdvantage />
       <OurServices />
-      <OurAddress />
+      {/* <OurAddress /> */}
       <Footer />
     </>
   );
